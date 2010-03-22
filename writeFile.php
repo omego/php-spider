04f1944a5445a422bf6f7c835e463bd1084c3c2b
Mazen A. Melibari <mazen@mazen.ws>
2010-03-21 21:02:02 -0400

    If condition to prevent writing in the file when there's no input

--- writeFile.php
+++ writeFile.php
@@ -6,11 +6,13 @@
 <body>
 
 <?php
-$myfile = "text.txt";
-$text = $_POST['url'];
-$file_write = fopen($myfile, 'w') or die("can't open file");
-fwrite($file_write, $text);
-fclose($file_write);
+if (isset($_POST['url']) && !empty($_POST['url'])) {
+	$myfile = "text.txt";
+	$text = $_POST['url'];
+	$file_write = fopen($myfile, 'w') or die("can't open file");
+	fwrite($file_write, $text);
+	fclose($file_write);
+}
 ?>
 
 <h4>Enter some text</h4>
