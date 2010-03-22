936f6ca31ddf5b40ffe2ffa24a54dc808bf69743
Mazen A. Melibari <mazen@mazen.ws>
2010-03-22 19:42:56 -0400

    A better way to do it

--- openFile.php
+++ openFile.php
@@ -4,8 +4,8 @@ $myfile = "text.txt";
 $file_read = fopen($myfile,'r');
 $thedata = fread($file_read, filesize($myfile));
 fclose($file_read);
-$thedata = explode(" " , $thedata);
-for ($start=0; $start < count($thedata); $start++) {
-print $thedata[$start] . "<BR>";
+
+for ($i=0; $i < strlen($thedata); $i++) {
+	echo $thedata[$i] . "<br />";
 }
 ?>
