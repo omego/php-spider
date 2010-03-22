b2be58da0e3701ad768623297499a407f26a4df7
omego <o.m.e.g.o@hotmail.com>
2010-03-23 02:12:08 +0300

    Files content added

--- openFile.php
+++ openFile.php
@@ -3,5 +3,8 @@ $myfile = "text.txt";
 $file_read = fopen($myfile,'r');
 $thedata = fread($file_read, filesize($myfile));
 fclose($file_read);
-echo $thedata;
+$thedata = explode(" " , $thedata);
+for ($start=0; $start < count($thedata); $start++) {
+print $thedata[$start] . "<BR>";
+}
 ?>
