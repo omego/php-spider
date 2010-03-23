c8cd83318f25fc2361d542a5ccf4dfd9e4cf76e6
omego <o.m.e.g.o@hotmail.com>
2010-03-23 03:14:51 +0300

    Files content added

---
+++ mySpider.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>spider</title>
+</head>
+
+<body>
+
+<h4>Enter URL</h4>
+<form action="mySpider.php" method="post"> 
+<input name="url" type="text" /> 
+<input type="submit" />
+</form>
+
+</body>
+</html>
