d6831b5bd85390197369ad7cdcb4629e534a80aa
omego <o.m.e.g.o@hotmail.com>
2010-03-23 03:22:18 +0300

    Files content added

--- mySpider.php
+++ mySpider.php
@@ -8,11 +8,9 @@
 if (isset($_POST['url']) && !empty($_POST['url'])) {
 	$myfile = "text.txt";
 	$text = $_POST['url'];
-	$file_write = fopen($myfile, 'w') or die("can't open file");
-	fwrite($file_write, $text);
-	fclose($file_write);
+    echo $text;
 }
-echo "$text"
+
 ?>
 <h4>Enter URL</h4>
 <form action="mySpider.php" method="post"> 
